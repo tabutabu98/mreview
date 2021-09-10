20df31e45fc3d0c7497edfe127f7627c5ae9b32c
syc <syc@partdb.com>
2021-09-10 16:32:16 +0900

    ReviewRepository 인터페이스에 '@EntityGraph' 추가 @syc

--- src/main/java/org/syc/mreview/review/repository/ReviewRepository.java
+++ src/main/java/org/syc/mreview/review/repository/ReviewRepository.java
@@ -1,5 +1,6 @@
 package org.syc.mreview.review.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.syc.mreview.movie.entity.Movie;
 import org.syc.mreview.review.entity.Review;
@@ -10,6 +11,8 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     // 신규 작성(379p), 특정 영화 번호를 이용해서 해당 영화를 리뷰한 정보
+    // 추가 작성(381p), @Entity 추가
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
     List<Review> findByMovie(Movie movie);
 
 }
