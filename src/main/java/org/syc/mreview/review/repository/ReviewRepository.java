0cc73548cebd0d3f6ee404f466e250f74422cf0f
syc <syc@partdb.com>
2021-09-10 16:15:04 +0900

    ReviewRepository 인터페이스에서 특정 영화 번호를 이용한 해당 영화를 리뷰한 정보 조회를 위한 메서드 추가 @syc

--- src/main/java/org/syc/mreview/review/repository/ReviewRepository.java
+++ src/main/java/org/syc/mreview/review/repository/ReviewRepository.java
@@ -1,8 +1,15 @@
 package org.syc.mreview.review.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.syc.mreview.movie.entity.Movie;
 import org.syc.mreview.review.entity.Review;
+
+import java.util.List;
 
 // 신규 작성(365p)
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    // 신규 작성(379p), 특정 영화 번호를 이용해서 해당 영화를 리뷰한 정보
+    List<Review> findByMovie(Movie movie);
+
 }
