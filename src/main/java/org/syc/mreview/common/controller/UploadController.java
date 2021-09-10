18fd2e6ff8be2b4ceeec7c6a87a65207df82f2db
syc <syc@partdb.com>
2021-09-10 18:20:48 +0900

    UploadController 클래스 생성 @syc

---
+++ src/main/java/org/syc/mreview/common/controller/UploadController.java
@@ -0,0 +1,27 @@
+package org.syc.mreview.common.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+// 신규 작성(388p)
+@RestController
+@Log4j2
+public class UploadController {
+
+    @PostMapping("/uploadAjax")
+    public void uploadFile(MultipartFile[] uploadFiles) {
+
+        for (MultipartFile uploadFile: uploadFiles) {
+
+            // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
+            String originalName =uploadFile.getOriginalFilename();
+            String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
+
+            log.info("fileName: " + fileName);
+        }   // end for
+
+    }   // end uploadFile
+
+}
