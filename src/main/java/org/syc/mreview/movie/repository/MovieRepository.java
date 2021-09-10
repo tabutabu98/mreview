aac158304f3bbc7fd22ef674a16a71618c87e6c0
syc <syc@partdb.com>
2021-09-10 13:26:28 +0900

    MovieRepository 인터페이스에 JPQL 추가 @syc

--- src/main/java/org/syc/mreview/movie/repository/MovieRepository.java
+++ src/main/java/org/syc/mreview/movie/repository/MovieRepository.java
@@ -1,8 +1,16 @@
 package org.syc.mreview.movie.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.syc.mreview.movie.entity.Movie;
 
 // 신규 작성(360p)
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    // JPQL(369p)
+    @Query("select m, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m" + "left outer join Review r on r.movie = m group by m")
+    Page<Object[]> getListPage(Pageable pageable);
+
 }
