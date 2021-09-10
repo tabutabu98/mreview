24f096536e0637d5171c4e9e45c49ac2fda3facd
syc <syc@partdb.com>
2021-09-10 16:02:08 +0900

    MovieRepository 인터페이스에 getMovieWithAll()의 JPQL 쿼리문 추가 수정 @syc

--- src/main/java/org/syc/mreview/movie/repository/MovieRepository.java
+++ src/main/java/org/syc/mreview/movie/repository/MovieRepository.java
@@ -31,7 +31,7 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 //    List<Object[]> getMovieWithAll(long mno);
 
     // getMovieWithAll() 수정(377p)
-    @Query("select m, mi " +
+    @Query("select m, mi, avg(coalesce(r.grade,0)), count(r) " +
             "from Movie m left outer join MovieImage mi on mi.movie = m " +
             "left outer join Review r on r.movie = m " +
             "where m.mno = :mno")
