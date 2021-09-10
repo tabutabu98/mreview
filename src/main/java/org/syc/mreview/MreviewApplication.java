be888ec550750e6385de23c51551bea4620387d5
syc <syc@partdb.com>
2021-09-10 12:58:20 +0900

    'MreviewApplication'에 '@EnableJpaAuditing' 주입 @syc

--- src/main/java/org/syc/mreview/MreviewApplication.java
+++ src/main/java/org/syc/mreview/MreviewApplication.java
@@ -2,8 +2,11 @@ package org.syc.mreview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// 신규 작성(351p), @EnableJpaAuditing 주입
 @SpringBootApplication
+@EnableJpaAuditing
 public class MreviewApplication {
 
     public static void main(String[] args) {
