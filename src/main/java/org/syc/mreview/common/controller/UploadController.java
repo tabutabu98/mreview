91e9c59aa568d3f5b0e97697eac40a90364f7d0f
syc <syc@partdb.com>
2021-09-13 12:01:07 +0900

    UploadController, 수정 및 업로드 이미지 출력하기(ResposeEntity) 추가 @syc

--- src/main/java/org/syc/mreview/common/controller/UploadController.java
+++ src/main/java/org/syc/mreview/common/controller/UploadController.java
@@ -2,16 +2,26 @@ package org.syc.mreview.common.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.syc.mreview.common.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 // 신규 작성(388p)
@@ -23,14 +33,58 @@ public class UploadController {
     @Value("${org.syc.upload.path}")    // application.yml의 변수
     private String uploadPath;
 
+//    @PostMapping("/uploadAjax")
+//    public void uploadFile(MultipartFile[] uploadFiles) {
+//
+//        for (MultipartFile uploadFile: uploadFiles) {
+//
+//            // 추가 작성(396p), 이미지 파일만 업로드 가능
+//            if(uploadFile.getContentType().startsWith("image") == false) {
+//                log.warn("this file is not image type");
+//            }   // 추가 작성끝
+//
+//            // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
+//            String originalName =uploadFile.getOriginalFilename();
+//            String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
+//
+//            log.info("fileName: " + fileName);
+//
+//            // 추가 작성(396p), 날짜 폴더 생성
+//            String folderPath = makeFolder();
+//
+//            // 추가 작성(396p), UUID
+//            String uuid = UUID.randomUUID().toString();
+//
+//            // 추가 작성(396p), 저장할 파일 이름 중간에 "_"를 이용해서 구분
+//            String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
+//
+//            Path savePath = Paths.get(saveName);
+//
+//            try{
+//                uploadFile.transferTo(savePath);
+//            }
+//            catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            // 추가 작성끝(396p)
+//
+//        }   // end for
+//
+//
+//    }   // end uploadFile
+
+    // 추가 수정(399p)
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles) {
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) {
+
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
 
         for (MultipartFile uploadFile: uploadFiles) {
 
             // 추가 작성(396p), 이미지 파일만 업로드 가능
             if(uploadFile.getContentType().startsWith("image") == false) {
                 log.warn("this file is not image type");
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }   // 추가 작성끝
 
             // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
@@ -51,16 +105,16 @@ public class UploadController {
             Path savePath = Paths.get(saveName);
 
             try{
-                uploadFile.transferTo(savePath);
+                uploadFile.transferTo(savePath);    // 실제 이미지 저장
+                resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             }
             catch (IOException e) {
                 e.printStackTrace();
             }
-            // 추가 작성끝(396p)
 
         }   // end for
 
-
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }   // end uploadFile
 
     // 추가 작성(397p), makeFolder() 메서드 정의
@@ -80,4 +134,33 @@ public class UploadController {
         return folderPath;
     }
 
+    // 업로드 이미지 출력하기, 신규 작성(401p)
+    @GetMapping("/display")
+    public ResponseEntity<byte[]>getFile(String fileName) {
+
+        ResponseEntity<byte[]> result = null;
+
+        try{
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+
+            log.info("fileName: " + srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            log.info("file: " + file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            // MIME타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            // 파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        }
+        catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return result;
+    }
 }
