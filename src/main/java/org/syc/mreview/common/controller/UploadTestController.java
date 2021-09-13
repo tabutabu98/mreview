09d55fb3cca287892135e84d858a7d33d7da1382
syc <syc@partdb.com>
2021-09-13 10:35:46 +0900

    UploadTestController, 신규 생성 @syc

---
+++ src/main/java/org/syc/mreview/common/controller/UploadTestController.java
@@ -0,0 +1,15 @@
+package org.syc.mreview.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// 신규 작성(389p)
+@Controller
+public class UploadTestController {
+
+    @GetMapping("/uploadEx")
+    public void uploadEx() {
+
+    }
+
+}
