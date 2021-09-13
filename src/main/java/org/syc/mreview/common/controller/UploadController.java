c9818d27b77155c7bb67ec3460e049f7c1a18610
syc <syc@partdb.com>
2021-09-13 10:35:09 +0900

    UploadController, application.yml의 Path 값 추가 @syc

--- src/main/java/org/syc/mreview/common/controller/UploadController.java
+++ src/main/java/org/syc/mreview/common/controller/UploadController.java
@@ -1,6 +1,7 @@
 package org.syc.mreview.common.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,6 +10,10 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @Log4j2
 public class UploadController {
+
+    // 신규 작성(394p)
+    @Value("${org.syc.upload.path}")    // application.yml의 변수
+    private String uploadPath;
 
     @PostMapping("/uploadAjax")
     public void uploadFile(MultipartFile[] uploadFiles) {
