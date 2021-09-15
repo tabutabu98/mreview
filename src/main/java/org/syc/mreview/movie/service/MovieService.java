6c7d8d5d8b75bdc351337622e01b90352ff18bd5
syc <syc@partdb.com>
2021-09-15 14:22:49 +0900

    MovieService, dto에서 Entity를 Map타입으로 반환하는 로직 추가 @syc

--- src/main/java/org/syc/mreview/movie/service/MovieService.java
+++ src/main/java/org/syc/mreview/movie/service/MovieService.java
@@ -1,8 +1,53 @@
 package org.syc.mreview.movie.service;
 
 import org.syc.mreview.movie.dto.MovieDTO;
+import org.syc.mreview.movie.dto.MovieImageDTO;
+import org.syc.mreview.movie.entity.Movie;
+import org.syc.mreview.movie.entity.MovieImage;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 // 신규 작성(420p)
+// 추가 수정(421p)
 public interface MovieService {
+
     Long register(MovieDTO movieDTO);
+
+    // Map타입으로 반환
+    default Map<String, Object> dtoToEntity(MovieDTO movieDTO){
+
+        Map<String, Object> entityMap = new HashMap<>();
+
+        Movie movie = Movie.builder()
+                .mno(movieDTO.getMno())
+                .title(movieDTO.getTitle())
+                .build();
+
+        entityMap.put("movie", movie);
+
+        List<MovieImageDTO> imageDTOList = movieDTO.getImageDTOList();
+
+        // MovieImageDTO처리
+        if(imageDTOList != null && imageDTOList.size() > 0){
+            List<MovieImage> movieImageList = imageDTOList.stream().map(movieImageDTO -> {
+
+                MovieImage movieImage = MovieImage.builder()
+                        .path(movieImageDTO.getPath())
+                        .imgName(movieImageDTO.getImgName())
+                        .uuid(movieImageDTO.getUuid())
+                        .movie(movie)
+                        .build();
+
+                return movieImage;
+            }).collect(Collectors.toList());
+
+            entityMap.put("imgList", movieImageList);
+        }
+
+        return entityMap;
+    }
+
 }
