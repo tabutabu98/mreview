6e3ce7737d008db75789365330a8b4de5a48c7e2
syc <syc@partdb.com>
2021-09-15 13:24:48 +0900

    MovieImageDTO, 생성 @syc

---
+++ src/main/java/org/syc/mreview/movie/dto/MovieImageDTO.java
@@ -0,0 +1,44 @@
+package org.syc.mreview.movie.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+// 신규 작성(419p)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MovieImageDTO {
+
+    private String uuid;
+
+    private String imgName;
+
+    private String path;
+
+    public String getImageURL(){
+        try{
+            return URLEncoder.encode(path+"/"+uuid+"_"+imgName,"UTF-8");
+        } catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public String getThumbnailURL(){
+        try {
+            return URLEncoder.encode(path+"/s_"+uuid+"_"+imgName, "UTF-8");
+        } catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+}
