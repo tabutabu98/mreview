8e14084feb8f99d24eeb61518af7e9d8a850856a
syc <syc@partdb.com>
2021-09-16 10:01:06 +0900

    MovieDTO, 영화의 편균 평점, 리뷰  수 jpa의 count, 생성일, 수정일 추가 @syc

--- src/main/java/org/syc/mreview/movie/dto/MovieDTO.java
+++ src/main/java/org/syc/mreview/movie/dto/MovieDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +16,26 @@ import java.util.List;
 @AllArgsConstructor
 public class MovieDTO {
 
+    // 영화 번호
     private Long mno;
 
+    // 영화 제목
     private String title;
 
     @Builder.Default
     private List<MovieImageDTO> imageDTOList = new ArrayList<>();
+
+    // 신규 작성(436p)
+    // 영화의 평균 평점
+    private double avg;
+
+    // 리뷰 수 jpa의 count()
+    private int reviewCnt;
+
+    // 생성일
+    private LocalDateTime regDate;
+
+    // 수정일
+    private LocalDateTime modDate;
 
 }
