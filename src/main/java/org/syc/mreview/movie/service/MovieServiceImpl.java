e5f598c12dd84774e05a76ca0a2f2c791f05cb21
syc <syc@partdb.com>
2021-09-16 14:33:50 +0900

    MovieServiceImpl, getMovie 추가 @syc

--- src/main/java/org/syc/mreview/movie/service/MovieServiceImpl.java
+++ src/main/java/org/syc/mreview/movie/service/MovieServiceImpl.java
@@ -15,6 +15,7 @@ import org.syc.mreview.movie.entity.MovieImage;
 import org.syc.mreview.movie.repository.MovieImageRepository;
 import org.syc.mreview.movie.repository.MovieRepository;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -66,5 +67,30 @@ public class MovieServiceImpl implements MovieService{
         );
 
         return new PageResultDTO<>(result, fn);
+    }
+
+    // 신규 작성(444p)
+    @Override
+    public MovieDTO getMovie(Long mno) {
+
+        List<Object[]> result = movieRepository.getMovieWithAll(mno);
+
+        // Movie 엔티티는 가장 앞에 존재 - 모든 Row가 동일한 값
+        Movie movie = (Movie) result.get(0)[0];
+
+        // 영화의 이미지 개수만큼 MovieImage객체 필요
+        List<MovieImage> movieImageList = new ArrayList<>();
+
+        result.forEach(arr -> {
+            MovieImage movieImage = (MovieImage) arr[1];
+            movieImageList.add(movieImage);
+        });
+
+        // 평균 평점 - 모든 Row가 동일한 값
+        Double avg = (Double) result.get(0)[2];
+        // 리뷰 개수 - 모든 Row가 동일한 값
+        Long reviewCnt = (Long) result.get(0)[3];
+
+        return entitiesToDTO(movie, movieImageList, avg, reviewCnt);
     }
 }
