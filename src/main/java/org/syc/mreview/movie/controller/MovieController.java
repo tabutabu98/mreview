8b5dddf05e590d834d4344743ac84747e33799ca
syc <syc@partdb.com>
2021-09-16 14:33:01 +0900

    MovieController, list, read, modify 겟맵핑 추가 @syc

--- src/main/java/org/syc/mreview/movie/controller/MovieController.java
+++ src/main/java/org/syc/mreview/movie/controller/MovieController.java
@@ -3,10 +3,13 @@ package org.syc.mreview.movie.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.syc.mreview.common.dto.PageRequestDTO;
 import org.syc.mreview.movie.dto.MovieDTO;
 import org.syc.mreview.movie.service.MovieService;
 
@@ -40,4 +43,25 @@ public class MovieController {
         return "redirect:/movie/list";
     }
 
+    // 신규 작성(438p)
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        log.info("pageRequestDTO: " + pageRequestDTO);
+
+        model.addAttribute("result", movieService.getList(pageRequestDTO));
+
+    }
+
+    // 신규 작성(445p), 리드페이지, 수정페이지의 겟맵핑
+    @GetMapping({"/read", "/modify"})
+    public void read(long mno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model) {
+
+        log.info("mno: " + mno);
+
+        MovieDTO movieDTO = movieService.getMovie(mno);
+
+        model.addAttribute("dto", movieDTO);
+
+    }
 }
